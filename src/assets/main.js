31fae4faff6b5b08a69fb374a6c493a3f228f399
TSYS098130 <34074150+TSYS098130@users.noreply.github.com>
2017-12-01 13:01:23 -0500

    Add files via upload

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,5 +1,11 @@
 $(function() {
 
-  // your code will go here
+ $.ajax({
+    url: 'https://www.codeschool.com/users/3737126',
+    dataType: 'jsonp',
+    success: function(response) {
+      // handle response
+    }
+  });
 
 });
